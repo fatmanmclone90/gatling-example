5f39fdbc5210c04b0a22460896397894f2b0bb52
chris_tamlyn <Chris_Tamlyn@next.co.uk>
2023-03-20 12:30:51 +0000

    Remove obsolete scenarioBuilders

--- Source/src/test/scala/com/next/simulations/LoadTest.scala
+++ Source/src/test/scala/com/next/simulations/LoadTest.scala
@@ -1,7 +1,7 @@
 package com.next.simulations
 
 import com.next.config.Configuration._
-import com.next.scenarioBuilders._
+import com.next.scenarios._
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 import scala.concurrent.duration._
@@ -11,20 +11,20 @@ class LoadTest extends Simulation {
   val _durationSeconds: Int = durationSeconds;
   
   // https://gatling.io/docs/gatling/reference/current/core/injection/
-  private val _mapSimulation = (new MapScenarioBuilder).Build()
-  .inject(
-    rampUsers(_numberOfMessages)
-    .during(_durationSeconds)
-  )
+   private val _validateSimulation = ValidateScenarios.validateRequestScenario
+    .inject(
+      rampUsers(_numberOfMessages)
+      .during(_durationSeconds)
+    )
 
-  private val _validateSimulation = (new ValidateScenarioBuilder).Build()
-  .inject(
-    rampUsers(_numberOfMessages)
-    .during(_durationSeconds)
-  )
+    private val _mapSimulation = MapScenarios.mapRequestScenario
+    .inject(
+      rampUsers(_numberOfMessages)
+      .during(_durationSeconds)
+    )
 
   // https://gatling.io/docs/gatling/reference/current/core/assertions/
-  setUp(_mapSimulation, _validateSimulation)
+  setUp(_validateSimulation, _mapSimulation)
     .assertions(global.responseTime.percentile(95).lt(1000))
     .assertions(global.failedRequests.percent.lt(1))
 }
