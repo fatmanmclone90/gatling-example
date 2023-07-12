fab0a87647d46aae4b23f2f21ca9ffdf826e9af4
chris_tamlyn <Chris_Tamlyn@next.co.uk>
2023-07-12 15:50:19 +0100

    Removed unused variables

--- Source/src/test/scala/com/example/config/Configuration.scala
+++ Source/src/test/scala/com/example/config/Configuration.scala
@@ -5,7 +5,5 @@ object Configuration {
   lazy val baseUrl: String = System.getProperty("baseUrl")
   lazy val numberOfMessages: Int = Integer.getInteger("numberOfMessages", 10).toInt
   lazy val durationSeconds: Int = Integer.getInteger("durationSeconds", 10).toInt
-  lazy val overallDuration: Double = System.getProperty("overallDuration").toDouble
-  lazy val headerCode: String = System.getProperty("headerCode")
   lazy val apiKey: String = System.getProperty("apiKey")
 }
